5fa07415338debb31dc029bbf204fbe34bde7d04
shehancv12346 <shehanop123@gmail.com>
2025-01-24 13:37:12 +0530

    Update config.js

--- config.js
+++ config.js
@@ -6,7 +6,7 @@ function convertToBool(text, fault = 'true') {
 }
 
 module.exports = {
-    SESSION_ID: process.env.SESSION_ID || "enter your session",
+    SESSION_ID: process.env.SESSION_ID || "NacH3Zab#R2gNi-Naqx1CB6csjpuRvO8DmjMEBpfiZLSUCmp44fM",
     CAPTION: process.env.CAPTION || "POWERED BY MR DINESH",
     AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
     READ_MESSAGE: process.env.READ_MESSAGE || "false", // Added auto-read configuration
